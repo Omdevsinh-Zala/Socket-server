fe72a224c32203fe2118a56a27f8b2000371d913
Omdevsinh-Zala <omdevsinh@krishaweb.com>
2025-01-10 14:42:17 +0530

    Fix issues

--- socket.js
+++ socket.js
@@ -7,35 +7,36 @@ import { Server } from './node_modules/socket.io/dist/index.js';
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-    }
+  cors: {
+    origin: "*",
+  }
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
-    res.sendFile(join(''));
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
 // Handle socket connections
 io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
-  
-    // Example: Receiving a message from the client
-    socket.on('message', (data) => {
-      console.log('Message received:', data);
-  
-      // Broadcast the message to other connected clients
-      io.emit('message', data)
-    });
-  
-    // Handle disconnection
-    socket.on('disconnect', () => {
-      console.log('User disconnected:', socket.id);
-    });
+  console.log('A user connected:', socket.id);
+
+  // Example: Receiving a message from the client
+  socket.on('message', (data) => {
+    console.log('Message received:', data);
+
+    // Broadcast the message to other connected clients
+    io.emit('message', data)
   });
 
-server.listen(3000, () => {
-    console.log('server running at http://192.168.1.141:3000');
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const port = process.env.PORT || 3300;
+server.listen(port, () => {
+  console.log(`Server running on ${port}`);
 });
